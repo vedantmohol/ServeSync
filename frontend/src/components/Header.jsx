75461dc9eefa988e546052a0e65c184899a33749
Vedant <vedantmohol18@gmail.com>
2025-03-17 23:04:00 +0530

    Updated header component for staff data

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -4,12 +4,11 @@ import React, { useState } from "react";
 import { Avatar, Button, Dropdown, Navbar, TextInput } from "flowbite-react";
 import { Link } from "react-router-dom";
 import ServeSyncLogo from "../assets/ServeSyncLogo.png";
-import { useSelector } from 'react-redux';
+import { useSelector } from "react-redux";
 
 function Header() {
   const [vegMode, setVegMode] = useState(true);
-  const {currentUser} = useSelector(state => state.user);
-
+  const { currentUser } = useSelector((state) => state.user);
 
   return (
     <Navbar className="bg-purple-800 text-white border-b-2 flex flex-wrap justify-between items-center p-4">
@@ -35,25 +34,91 @@ function Header() {
 
       <div className="flex items-center gap-4">
         {currentUser && (
-          <Button
-            gradientDuoTone="purpleToBlue"
-            className="px-4 py-2"
-            onClick={() => setVegMode(!vegMode)}
-          >
-            {vegMode ? (
-              <FaLeaf className="text-green-500" />
-            ) : (
-              <FaDrumstickBite className="text-red-500" />
+          <>
+            {currentUser.role === "customer" && (
+              <>
+                <Button
+                  gradientDuoTone="purpleToBlue"
+                  className="px-4 py-2"
+                  onClick={() => setVegMode(!vegMode)}
+                >
+                  {vegMode ? (
+                    <FaLeaf className="text-green-500" />
+                  ) : (
+                    <FaDrumstickBite className="text-red-500" />
+                  )}
+                </Button>
+                <Link to={"/book-table"}>
+                  <Button gradientDuoTone="purpleToBlue" className="px-4 py-2">
+                    Book Table
+                  </Button>
+                </Link>
+                <Link to="/bill">
+                  <Button gradientDuoTone="purpleToBlue" className="px-4 py-2">
+                    Pay Bill
+                  </Button>
+                </Link>
+              </>
             )}
-          </Button>
-        )}
 
-        {currentUser && (
-          <Link to="/bills">
-            <Button gradientDuoTone="purpleToBlue" className="px-4 py-2">
-              Bill
-            </Button>
-          </Link>
+            {currentUser.role === "hall_manager" && (
+              <>
+                <Link to={"/take-order"}>
+                  <Button gradientDuoTone="purpleToBlue" className="px-4 py-2">
+                    Take Orders
+                  </Button>
+                </Link>
+                <Link to={"/generate-bill"}>
+                  <Button gradientDuoTone="purpleToBlue" className="px-4 py-2">
+                    Generate Bills
+                  </Button>
+                </Link>
+                <Link to={"/manage-table"}>
+                  <Button gradientDuoTone="purpleToBlue" className="px-4 py-2">
+                    Manage Tables
+                  </Button>
+                </Link>
+              </>
+            )}
+
+            {currentUser.role === "hotel_admin" && (
+              <>
+                <Link to={"/admin-bills"}>
+                  <Button gradientDuoTone="purpleToBlue" className="px-4 py-2">
+                    View Bill
+                  </Button>
+                </Link>
+                <Link to={"/staff"}>
+                  <Button gradientDuoTone="purpleToBlue" className="px-4 py-2">
+                    View Staff
+                  </Button>
+                </Link>
+              </>
+            )}
+
+            {currentUser.role === "chef" && (
+              <>
+                <Link to={"/orders-chef"}>
+                  <Button gradientDuoTone="purpleToBlue" className="px-4 py-2">
+                    View Orders
+                  </Button>
+                </Link>
+                <Link to={"/food-stock"}>
+                  <Button gradientDuoTone="purpleToBlue" className="px-4 py-2">
+                    Food Stock
+                  </Button>
+                </Link>
+              </>
+            )}
+
+            {currentUser.role === "waiter" && (
+              <Link to="/mark-availability">
+                <Button gradientDuoTone="purpleToBlue" className="px-4 py-2">
+                  Mark Availability
+                </Button>
+              </Link>
+            )}
+          </>
         )}
 
         {currentUser ? (
@@ -70,7 +135,7 @@ function Header() {
                 {currentUser.email}
               </span>
             </Dropdown.Header>
-            <Link to={'/dashboard?tab=profile'}>
+            <Link to={"/dashboard?tab=profile"}>
               <Dropdown.Item>Profile</Dropdown.Item>
             </Link>
             <Dropdown.Divider />
