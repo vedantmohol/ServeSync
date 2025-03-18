8f6f206f7f22d5f971bd2e61528a7fbcbbe1b370
Vedant <vedantmohol18@gmail.com>
2025-03-18 22:45:41 +0530

    Completed customer dashboard component

---
+++ frontend/src/components/DashSidebar.jsx
@@ -0,0 +1,30 @@
+import { Sidebar } from 'flowbite-react'
+import React, { useEffect, useState } from 'react'
+import { HiUser, HiArrowSmRight } from "react-icons/hi"
+import { Link, useLocation } from 'react-router-dom';
+
+function DashSidebar() {
+    const location = useLocation();
+    const [tab,setTab] = useState('');
+
+    useEffect(()=>{
+        const urlParams = new URLSearchParams(location.search);
+        const tabFromUrl = urlParams.get('tab');
+        setTab(tabFromUrl);
+    },[location.search]);
+
+  return (
+    <Sidebar className='md:w-56 w-full'>
+        <Sidebar.Items>
+            <Sidebar.ItemGroup>
+                <Link to="/dashboard?tab=profile">
+                <Sidebar.Item active={tab === '/profile'} icon={HiUser} label='User' labelColor="dark">Profile</Sidebar.Item>
+                </Link>
+                <Sidebar.Item icon={HiArrowSmRight} classname="cursor-pointer">SignOut</Sidebar.Item>
+            </Sidebar.ItemGroup>
+        </Sidebar.Items>
+    </Sidebar>
+  )
+}
+
+export default DashSidebar
