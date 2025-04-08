53417c7e8c97c5c122c8c320473ec774492f28f3
Vedant <vedantmohol18@gmail.com>
2025-04-08 13:19:07 +0530

    Created and added food card to home page

---
+++ frontend/src/components/FoodCard.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { Modal, Button } from 'flowbite-react';
+
+export default function FoodCard({ item }){
+    const { food, hotelName, hotelAddress } = item;
+    const [showModal, setShowModal] = useState(false);
+    const navigate = useNavigate();
+    const { currentUser } = useSelector((state) => state.user);
+
+    const handleAddToCart = () => {
+        if (!currentUser) {
+          setShowModal(true);
+        } else {
+          console.log('Added to cart:', item.food.name);
+        }
+      };
+
+
+    return (
+        <>
+        <div className='group relative w-full border border-teal-500 hover:border-2 h-[370px] overflow-hidden rounded-lg sm:w-[300px] md:w-[320px] transition-all mx-auto'>
+          <img
+            src={food.image}
+            alt='Food'
+            className='h-[260px] w-full object-cover group-hover:h-[200px] transition-all duration-300 z-20'
+          />
+          <div className='p-3 flex flex-col gap-2'>
+            <p className='text-lg font-semibold line-clamp-1'>{food.name}</p>
+            <div className='text-sm text-black-600 mt-auto'>
+              <p>{hotelName} 
+                <span className=' text-sm text-gray-500 m-2'>({food.dishType}) </span>
+              </p>
+            </div>
+            <p className='font-medium'>₹ {food.price}</p>
+            <button
+            onClick={handleAddToCart}
+            className='mt-2 border border-teal-500 text-teal-500 hover:bg-teal-500 hover:text-white transition-all duration-300 text-center py-2 rounded-md'
+          >
+            Add to Cart
+          </button>
+          </div>
+        </div>
+        <Modal show={showModal} onClose={() => setShowModal(false)} size="md" popup>
+        <Modal.Header />
+        <Modal.Body>
+          <div className="text-center">
+            <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
+              Please login to order.
+            </h3>
+            <div className="flex justify-center gap-4">
+              <Button color="success" onClick={() => navigate('/sign-in')}>
+                Sign In
+              </Button>
+              <Button color="gray" onClick={() => setShowModal(false)}>
+                Go Back
+              </Button>
+            </div>
+          </div>
+        </Modal.Body>
+      </Modal>
+      </>
+      );
+}
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import FoodCard from '../components/FoodCard';
 
 function Home() {
+  const [foods, setFoods] = useState([]);
+
+  useEffect(() => {
+    const fetchFoods = async () => {
+      try {
+        const res = await fetch('/api/food/get');
+        const data = await res.json();
+        setFoods(data.foods);
+      } catch (error) {
+        console.error('Error fetching food:', error);
+      }
+    };
+
+    fetchFoods();
+  }, []);
+
   return (
-    <div>Home</div>
-  )
+    <div className='max-w-6xl mx-auto p-3 flex flex-col gap-8 py-3'>
+      {foods && foods.length > 0 && (
+        <div className='flex flex-col gap-6'>
+          <h2 className='text-2xl font-semibold mt-10'>DISHES RECOMMENDED FOR YOU</h2>
+          <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 place-items-center'>
+            {foods.map((item, idx) => (
+              <FoodCard key={idx} item={item} />
+            ))}
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Home
