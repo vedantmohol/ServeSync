10786fa45b3c131dfca56d5c8d01bd4858b1d285
Vedant <vedantmohol18@gmail.com>
2025-04-11 20:24:13 +0530

    Updated hotel model for adding tables and hotel structure

--- backend/models/hotel.model.js
+++ backend/models/hotel.model.js
@@ -1,5 +1,39 @@
 import mongoose from "mongoose";
 
+const tableSchema = new mongoose.Schema({
+  tableId: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  isPremium: {
+    type: String,
+    enum: ["Yes", "No"],
+    default: "No",
+  },
+  isBooked: {
+    type: String,
+    enum: ["Yes", "No"],
+    default: "No",
+  },
+  capacity: {
+    type: Number,
+    default: 2,
+  },
+  charges: {
+    type: Number,
+  }
+}, { _id: false });
+
+const floorSchema = new mongoose.Schema({
+  floorId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  tables: [tableSchema]
+}, { _id: false });
+
 const hotelSchema = new mongoose.Schema({
     adminName: {
         type: String,
@@ -39,6 +73,13 @@ const hotelSchema = new mongoose.Schema({
         type: Number,
         default: 0,
     },
+    numberOfWaiters: {
+        type: Number,
+        default: 0,
+    },
+    numberOfHallManagers: {
+        type: Number,
+        default: 0,
     chefs: [
         {
           name: String,
@@ -75,14 +116,12 @@ const hotelSchema = new mongoose.Schema({
           },
         }
       ],
-    numberOfWaiters: {
-        type: Number,
-        default: 0,
     },
-    numberOfHallManagers: {
-        type: Number,
-        default: 0,
+    numberOfFloors: {
+      type: Number,
+      default: 0,
     },
+    floors: [floorSchema],
     hotelId: {
         type: String,
         unique: true,
